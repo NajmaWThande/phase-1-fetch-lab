2b783f122e92ac6483c8c959b898835a63d64251
NajmaWThande <najmathande@gmail.com>
2023-04-04 12:30:55 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function fetchBooks() {
+function fetchBooks() { 
+
   // To pass the tests, don't forget to return your fetch!
+  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then(json => renderBooks(json))
+ 
   
 }
 
@@ -9,7 +15,7 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
